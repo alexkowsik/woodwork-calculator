87bcbb039fe1e66dba9b6b0b32beb027f57d7f6f
Alexander Kowsik <alexander.kowsik@gmx.de>
2022-06-24 22:31:51 +0200

    Bugfixes

--- scripts.js
+++ scripts.js
@@ -14,7 +14,11 @@ function draw() {
     }
 
     var angle = 1/2 * (180 - 360 / num);
-    var thickness_actual_tmp = Math.abs(thickness / Math.sin(angle));
+    var thickness_actual_tmp = thickness / Math.sin(angle * (Math.PI/180));
+
+    console.log(Math.sin(angle));
+    console.log(thickness)
+    console.log(thickness_actual_tmp)
 
     var width_mult = diameter / (2 * width);
     thickness_actual = thickness_actual_tmp / width_mult;
