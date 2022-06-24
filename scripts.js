b0f3a98f4549b9cd79ada78de9808fbc443dcd8a
Alexander Kowsik <alexander.kowsik@gmx.de>
2022-06-24 22:44:57 +0200

    Refactoring

--- scripts.js
+++ scripts.js
@@ -1,7 +1,5 @@
 function draw() {
     const canvas = document.getElementById('canvas');
-    const width = canvas.width / 2;
-
     var thickness = document.getElementById('thickness').value;
     var diameter = document.getElementById('diameter').value;
     var num = document.getElementById('num').value;
@@ -13,24 +11,24 @@ function draw() {
         document.getElementById("output").innerHTML = "";
     }
 
+    const width = canvas.width / 2;
+    var width_div = diameter / (2 * width);
     var angle = 1/2 * (180 - 360 / num);
-    var thickness_actual_tmp = thickness / Math.sin(angle * (Math.PI/180));
-
-    var width_mult = diameter / (2 * width);
-    thickness_actual = thickness_actual_tmp / width_mult;
+    var thickness_actual = thickness / Math.sin(angle * (Math.PI/180) * width_div);
 
     if (thickness_actual > width) {
         document.getElementById("output").innerHTML = "Brett zu dick bei diesem Durchmesser";
         return;
     } else {
         document.getElementById("output").innerHTML = "";
+        thickness_actual = Math.min(thickness_actual, width)
     }
 
-    var breadth = 2 * thickness_actual_tmp * Math.tan(Math.PI / num)
+    var breadth = 2 * thickness_actual * width_div * Math.tan(Math.PI / num)
 
-    document.getElementById("output").innerHTML = "Der Winkel <strong>α</strong> beträgt " + Math.round(angle * 100) / 100 + "°" + " und die Breite eines Bretts beträgt " + Math.round(breadth * 100) / 100 + "mm.";
-
-    thickness_actual = Math.min(thickness_actual, width)
+    document.getElementById("output").innerHTML = "Der Winkel <strong>α</strong> beträgt " 
+        + Math.round(angle * 100) / 100 + "°" + " und die Breite eines Bretts beträgt " 
+        + Math.round(breadth * 100) / 100 + "mm.";
 
     if (canvas.getContext) {
         const ctx = canvas.getContext('2d');
@@ -42,49 +40,39 @@ function draw() {
         ctx.stroke();
 
         // polygon 1
-        var numberOfSides = num,
-        size = width,
+        var size = width,
         Xcenter = width,
         Ycenter = width;
 
         ctx.beginPath();
-        ctx.moveTo (Xcenter +  size * Math.cos(0), Ycenter +  size *  Math.sin(0));          
+        ctx.moveTo(Xcenter +  size * Math.cos(0), Ycenter +  size *  Math.sin(0));          
 
-        for (var i = 1; i <= numberOfSides;i += 1) {
-            ctx.lineTo (Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides), Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides));
+        for (var i = 1; i <= num;i += 1) {
+            ctx.lineTo(Xcenter + size * Math.cos(i * 2 * Math.PI / num), Ycenter + size * Math.sin(i * 2 * Math.PI / num));
         }
 
-        ctx.lineWidth = 1;
         ctx.stroke();
 
         // polygon 2
-        var numberOfSides = num,
-        size = width - thickness_actual,
-        Xcenter = width,
-        Ycenter = width;
+        size = width - thickness_actual;
 
         ctx.beginPath();
-        ctx.moveTo (Xcenter +  size * Math.cos(0), Ycenter +  size *  Math.sin(0));          
+        ctx.moveTo(Xcenter +  size * Math.cos(0), Ycenter +  size *  Math.sin(0));          
 
-        for (var i = 1; i <= numberOfSides;i += 1) {
-            ctx.lineTo (Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides), Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides));
+        for (var i = 1; i <= num;i += 1) {
+            ctx.lineTo(Xcenter + size * Math.cos(i * 2 * Math.PI / num), Ycenter + size * Math.sin(i * 2 * Math.PI / num));
         }
 
-        ctx.lineWidth = 1;
         ctx.stroke();
 
         // Inner lines
-        var numberOfSides = num,
-        size = width,
-        Xcenter = width,
-        Ycenter = width;
+        size = width;
 
-        for (var i = 0; i <= numberOfSides;i += 1) {
+        for (var i = 0; i <= num;i += 1) {
             ctx.beginPath();
-            ctx.moveTo (Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides), Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides));
-            ctx.lineTo (Xcenter + (size - thickness_actual) * Math.cos(i * 2 * Math.PI / numberOfSides), Ycenter + (size - thickness_actual) * Math.sin(i * 2 * Math.PI / numberOfSides));
+            ctx.moveTo(Xcenter + size * Math.cos(i * 2 * Math.PI / num), Ycenter + size * Math.sin(i * 2 * Math.PI / num));
+            ctx.lineTo(Xcenter + (size - thickness_actual) * Math.cos(i * 2 * Math.PI / num), Ycenter + (size - thickness_actual) * Math.sin(i * 2 * Math.PI / num));
             ctx.stroke();
         }
-        
     }
   }
